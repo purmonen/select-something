e896c81de03c0280a9379b50ce29820ef406f250
Sami Purmonen <purmonen@kth.se>
2013-10-06 06:15:12 +0200

    Update

--- selection.js
+++ selection.js
@@ -6,6 +6,7 @@ app.controller('SelectionCtrl', function($scope, $timeout) {
 	$scope.element = '';
 	$scope.selectionSize = 0;
 	$scope.showNoSelection = true;
+	$scope.focus = true;
 	var allowSelect = true;
 
 	$scope.addElement = function() {
@@ -45,37 +46,63 @@ app.controller('SelectionCtrl', function($scope, $timeout) {
 			allowSelect = false;
 			$scope.showNoSelection = false;
 			$scope.selected = [];
-			select([]);
+			highLight(10, $('#select-from').children(), 0, 150, function() {
+				select([]);
+			});
+		}
+	}
+
+	function highLight(count, children, last, timeout, func) {
+		if (count > 0) {
+			while (true) {
+				i = randomRange(0, $scope.elements.length - 1);
+				if (i != last) {
+					break;
+				}
+			}
+			console.log(i);
+			e = children.eq(i);
+			e.addClass('highlight');
+			count -= 1;
+			$timeout(function() {
+				e.removeClass('highlight');
+				highLight(count, children, i, timeout, func);
+			},  timeout);
+		} else {
+			func();
 		}
 	}
 
 	function select(indexes) {
-		var timeout = 500;
+		var timeout = 250;
 		if (indexes.length < $scope.selectionSize) {
 			while (true) {
 				random = randomRange(0, $scope.elements.length-1);
 				if (indexes.indexOf(random) === -1) {
 					indexes.push(random);
-					$('#select-from').children().eq(random).animate({
+					var e = $('#select-from').children().eq(random);
+					e.addClass('highlight')
+					e.animate({
 						position: 'absolute',
-						left: '100%'
+						left: '100%',
+						opacity: 0.8,
 					}, timeout).animate({
-						left: '0'
-					}, 0);
+						left: '0',
+						opacity: 1
+					}, 0, function() {
+						e.removeClass('highlight');
+					});
 					break;
 				}
 			}
-			select(indexes);
-		} else {
-			$timeout(function () {
-				for (i = 0; i < indexes.length; i++) {
-					$scope.selected.push($scope.elements[indexes[i]]);
-				}
-				allowSelect = true;
+			$timeout(function() {
+				$scope.selected.push($scope.elements[indexes[indexes.length-1]]);
+				select(indexes);
 			}, timeout);
+		} else {
+			allowSelect = true;
 		}
 	}
-
 
 	function randomRange(min, max) {
 		return Math.floor(Math.random() * (max-min+1)) + min;
